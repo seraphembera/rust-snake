2a6ad4e28e97b72252604a38daf5809d11611080
seraphembera <2607068868@qq.com>
2024-05-24 21:03:30 +0800

    Create start window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use fltk::{app, prelude::*, window, frame::Frame, button::Button};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut wind = window::Window::new(100, 100, 600, 400, "Snake");
+    let mut but_start = Button::new(260, 150, 80, 40, "Start Game");
+    let mut but_exit = Button::new(260, 250, 80, 40, "Exit");
+    let mut frame = Frame::default().with_size(200, 100).center_of(&wind);
+
+    wind.end();
+    wind.show();
+
+    but_start.set_callback(move |_| frame.set_label("hello world"));
+    but_exit.set_callback(move |_| process::exit(1));
+    app.run().unwrap();
 }
